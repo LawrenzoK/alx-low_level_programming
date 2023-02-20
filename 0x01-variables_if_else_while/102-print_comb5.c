4cb542ffefa9ae43244cd7562c26d46601da5aa2
LawrenzoK <slkdesignsa@gmail.com>
2023-02-20 07:51:53 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,32 +6,25 @@
  */
 int main(void)
 {
-	int i, j, firstDigit_i, secondDigit_i, firstDigit_j, secondDigit_j;
+	int i, j;
 
-	for (i = 0; i <= 99; i++)
-	{
-		firstDigit_i = i / 10;
-		secondDigit_i = i % 10;
-			for (j = i; j <= 99; j++)
-			{
-				firstDigit_j = j / 10;
-				secondDigit_j = j % 10;
-				if (i != j)
+	for (i = 0; i < 100; i++)
+		for (j = i; j < 100; j++)
+		{
+			putchar('0' + (i / 10));
+			putchar('0' + (i % 10));
+			putchar(' ');
+			putchar('0' + (j / 10));
+			putchar('0' + (j % 10);
+				if (i == 99 && j == 99)
 				{
-					putchar('0' + firstDigit_i);
-					putchar('0' + secondDigit_i);
+					putchar('\n');
+				} else
+				{
+					putchar(',');
 					putchar(' ');
-					putchar('0' + firstDigit_j);
-					putchar('0' + secondDigit_j);
-					if (i != 99 || j != 99)
-					{
-						putchar(',');
-						putchar(' ');
-					}
 				}
 			}
-	}
-	putchar('\n');
-
+		}
 	return (0);
 }
