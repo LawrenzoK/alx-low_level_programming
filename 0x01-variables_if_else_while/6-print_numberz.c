cce1ecf46745314424264d6f41093d7b10c60fb3
LawrenzoK <slkdesignsa@gmail.com>
2023-02-20 06:22:18 -0800

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,16 +6,13 @@
  */
 int main(void)
 {
-	putchar('0');
-	putchar('1');
-	putchar('2');
-	putchar('3');
-	putchar('4');
-	putchar('5');
-	putchar('6');
-	putchar('7');
-	putchar('8');
-	putchar('9');
+	int num = 48;
+
+	while (num < 58)
+	{
+		putchar(num);
+		num++;
+	}
 	putchar('\n');
 
 	return (0);
