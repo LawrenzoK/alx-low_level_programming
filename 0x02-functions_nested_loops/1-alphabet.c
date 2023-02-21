19d5d0bb69b2dfa1f6bd6ffa18b1f2ab6e6cd7f3
LawrenzoK <slkdesignsa@gmail.com>
2023-02-21 08:01:57 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <main.h>
+
+/**
+ *main -Entry point
+ *Return: Always 0 (success)
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+	do
+	{
+		putchar(c);
+		c++;
+	}
+	while(c <= 'z');
+}
+
+int main(void)
+{
+	print_alphabet();
+
+	return (0);
+}
