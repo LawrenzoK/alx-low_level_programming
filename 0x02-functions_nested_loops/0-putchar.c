ce9e5ba1cb9e2ee525968cea207255d8f6d5c561
LawrenzoK <slkdesignsa@gmail.com>
2023-02-20 23:08:34 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *main -Entry point
+ *Return: Always 0 (success)
+ */
+void printPutchar(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+}
+
+int main(void)
+{
+	printPutchar();
+
+	return (0);
+}
