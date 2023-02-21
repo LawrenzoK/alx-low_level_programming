fc5f84161994988d099e3e964b7d53d43cea8586
LawrenzoK <slkdesignsa@gmail.com>
2023-02-21 14:11:30 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,6 +3,8 @@
 /**
  * print_number - prints all natural numbers from n to 98
  * @n: starting number
+ *
+ * Return: void
  */
 void print_number(int n)
 {
@@ -11,15 +13,18 @@ void print_number(int n)
 		print_char('-');
 		n = -n;
 	}
-	if (n / 10)
+	if (n / 10 != 0)
 	{
 		print_number(n / 10);
+		print_char((n % 10) + '0');
 	}
 }
 
 /**
  * print_char - prints all natural numbers from n to 98
  * @c: starting number
+ *
+ * Return: void
  */
 void print_char(char c)
 {
@@ -61,28 +66,4 @@ void print_to_98(int n)
 		}
 	}
 	print_char('\n');
-}
-
-/**
- * print_number - prints a number using _putchar
- * @n: number to print
- *
- * Return: void
- */
-void print_number(int n)
-{
-	if (n / 10 != 0)
-		print_number(n / 10);
-	print_char((n % 10) + '0');
-}
-
-/**
- * print_char - writes a character to stdout
- * @c: the character to print
- *
- * Return: void
- */
-void print_char(char c)
-{
-	write(1, &c, 1);
 }
