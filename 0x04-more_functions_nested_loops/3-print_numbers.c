3567804c37462ffdcf629d23e2844018f1c90306
LawrenzoK <slkdesignsa@gmail.com>
2023-02-23 12:47:42 -0800

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * print_number - prints all natural numbers from 0 to 98
+ * print_numbers- prints all natural numbers from 0 to 98
  *
  * Return: void
  */
 
-void print_number(void)
+void print_numbers(void)
 {
 	int i;
 
@@ -14,4 +14,5 @@ void print_number(void)
 	{
 		printf("%d\n", i);
 	}
+	printf("\n");
 }
