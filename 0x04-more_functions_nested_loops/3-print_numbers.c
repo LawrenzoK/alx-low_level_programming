3a200aab587fc8016f63a4f9df6135d5169102d5
LawrenzoK <slkdesignsa@gmail.com>
2023-02-23 12:44:45 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_number - prints all natural numbers from 0 to 98
+ *
+ * Return: void
+ */
+
+void print_number(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,10 +5,13 @@ int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
+
 int _isupper(int c);
 int _isalpha(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
+
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
