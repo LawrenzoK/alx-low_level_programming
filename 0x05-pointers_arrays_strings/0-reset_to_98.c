dc19511d37050ca5d36f799781ca73a2af86d6ca
LawrenzoK <slkdesignsa@gmail.com>
2023-02-28 11:14:07 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include <main.h>
+
+/**
+ * reset_to_98 - update value
+ * @n: value to return
+ * return : 0
+ */
+
+void reset_to_98(int *n)
+{
+	*ptr = 98;
+
+	int x = 0;
+	int *ptr = &x;
+
+	return (0);
+}
