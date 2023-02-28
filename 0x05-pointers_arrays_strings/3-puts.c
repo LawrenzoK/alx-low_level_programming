e8749e4dfea591b3cf15982d742df1b841658560
LawrenzoK <slkdesignsa@gmail.com>
2023-02-28 12:01:40 -0800

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,15 +3,17 @@
 /**
  * _puts - Print a string to the standard output
  *
- * @s:  Pointer to the first character
+ * @str:  Pointer to the first character
  */
 
 void _puts(char *str)
 {
-	while (*s != '\0')
+	int i = 0;
+
+	while (*str[i] != '\0')
 	{
-		putchar(*s);
-		s++;
+		putchar(str[i]);
+		i++;
 	}
 	putchar('\n');
 }
