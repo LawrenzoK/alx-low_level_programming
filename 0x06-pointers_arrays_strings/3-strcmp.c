a57721134c4c178874459f45492a040c3e760b33
LawrenzoK <slkdesignsa@gmail.com>
2023-03-02 12:02:49 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,6 +2,8 @@
 
 /**
  * _strcmp - compare string
+ *@s1: string 1
+ *@s2: string 2
  *
  * Return: compared string
  */
@@ -9,9 +11,10 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
+
 	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
 	{
 		i++;
 	}
-	return s1[i] - s2[i];
+	return (s1[i] - s2[i]);
 }
