1ec23fb79a798a19c4e5e7cf8f889a3e66b6a22c
LawrenzoK <slkdesignsa@gmail.com>
2023-03-06 12:22:21 -0800

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strchr - locate character in string
+ * @s: pointer to string to search
+ * @c: character to search for
+ *
+ * Return: pointer to first occurrence
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (c == '\0')
+	{
+		return (s);
+	}
+	return (NULL);
+}
