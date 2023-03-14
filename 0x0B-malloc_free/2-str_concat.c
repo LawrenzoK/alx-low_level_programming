943359cb5d7f5b6208cdd1feefafa2fd219bfaba
LawrenzoK <slkdesignsa@gmail.com>
2023-03-14 22:27:36 +0200

    Updated codes

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,7 +12,7 @@ char *str_concat(char *s1, char *s2)
 {
 	char *concat_str;
 	int s1_len = 0, s2_len = 0, i, j;
-	
+
 	if (s1 == NULL)
 	{
 		s1 = "";
@@ -21,7 +21,7 @@ char *str_concat(char *s1, char *s2)
 	{
 		s2 = "";
 	}
-	
+
 	while (s1[s1_len] != '\0')
 	{
 		s1_len++;
@@ -31,12 +31,12 @@ char *str_concat(char *s1, char *s2)
 		s2_len++;
 	}
 	concat_str = malloc((s1_len + s2_len + 1) * sizeof(char));
-	
+
 	if (concat_str == NULL)
 	{
 		return (NULL);
 	}
-	
+
 	for (i = 0; i < s1_len; i++)
 	{
 		concat_str[i] = s1[i];
@@ -46,6 +46,6 @@ char *str_concat(char *s1, char *s2)
 		concat_str[i + j] = s2[j];
 	}
 		concat_str[i + j] = '\0';
-	
+
 	return (concat_str);
 }
---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - Allocates memory
+ * 
+ * @width: The width of the array.
+ * @height: The height of the array.
+ *
+ * Return: If memory allocation fails, return NULL.
+ *         Otherwise, return a pointer to the allocated 2D array.
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **grid;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	
+	grid = malloc(height * sizeof(int *));
+	
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+	
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+        {
+            for (j = i - 1; j >= 0; j--)
+                free(grid[j]);
+            free(grid);
+            return (NULL);
+        }
+        for (j = 0; j < width; j++)
+            grid[i][j] = 0;
+    }
+
+    return (grid);
+}
