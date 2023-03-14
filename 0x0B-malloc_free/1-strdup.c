70b3a0a7c8ad9977ddf3e7c8adde87ec1b4ed03e
LawrenzoK <slkdesignsa@gmail.com>
2023-03-14 13:01:28 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,46 +2,37 @@
 #include <stdlib.h>
 
 /**
- * str_concat - concatenates two strings
- * @s1: first string
- * @s2: second string
- *
- * Return: pointer to new string
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * @str: string to duplicate
+ * Return: pointer to duplicated string
  */
 
-char *str_concat(char *s1, char *s2)
+char *_strdup(char *str)
 {
-	char *new_str;
-	unsigned int len1, len2, i, j;
+	char *dup_str;
+	int i, len = 0;
 
-	if (s1 == NULL)
-	{
-		s1 = "";
-	}
-	if (s2 == NULL)
-	{
-		s2 = "";
-	}
-
-	for (len1 = 0; s1[len1] != '\0'; len1++)
-	for (len2 = 0; s2[len2] != '\0'; len2++)
-
-	new_str = malloc(sizeof(char) * (len1 + len2 + 1));
-
-	if (new_str == NULL)
+	if (str == NULL)
 	{
 		return (NULL);
 	}
 
-	for (i = 0; i < len1; i++)
+	while (str[len] != '\0')
 	{
-		new_str[i] = s1[i];
+		len++;
 	}
-	for (j = 0; j < len2; j++)
-	{
-		new_str[i + j] = s2[j];
-	}
-	new_str[i + j] = '\0';
 
-	return (new_str);
+	dup_str = malloc(sizeof(char) * (len + 1));
+
+	if (dup_str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i <= len; i++)
+	{
+		dup_str[i] = str[i];
+	}
+
+	return (dup_str);
 }
