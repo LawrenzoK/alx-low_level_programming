593cdd06777138da3027873f81fee5a396a42e0b
LawrenzoK <slkdesignsa@gmail.com>
2023-03-14 22:10:44 +0200

    main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,10 +2,7 @@
 #define MAIN_H
 
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
-int _strlen(char *s);
-char *_memcpy(char *dest, char *src, unsigned int n);
-void _puts(char *s);
-int _putchar(char c);
 
 #endif
