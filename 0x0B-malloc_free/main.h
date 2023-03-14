1f0837b00ad61897053fd2b93597491cee6318a1
LawrenzoK <slkdesignsa@gmail.com>
2023-03-14 12:51:47 -0700

    1-strdup.c

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,5 +2,10 @@
 #define MAIN_H
 
 char *create_array(unsigned int size, char c);
+char *str_concat(char *s1, char *s2);
+int _strlen(char *s);
+char *_memcpy(char *dest, char *src, unsigned int n);
+void _puts(char *s);
+int _putchar(char c);
 
 #endif
